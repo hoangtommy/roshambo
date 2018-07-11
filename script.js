0c695ee2265fa26a55a197bf2bf44c1e2e254961
Tommy <t@tommyhoang.com>
2018-07-11 14:56:23 -0700

    UI added. Game moved off console to webpage

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+	button.addEventListener('click', playGame)//(e) => {
+		// console.log(e.target.id);
+		// if (e.target.id == 'rock')
+	// });
+});
+
+let tieMessage = `Tie Time!`;
+let winMessage = `You win!`;
+let loseMessage = `You lose!`
+
 // Computer's hand:
 function computerHand() {
 	let number = Math.round(Math.random()*2 + 1);
@@ -12,61 +24,117 @@ function computerHand() {
 	}
 }
 
-// User's hand:
-function playerHand() {
-	let response = prompt("enter rock, paper, or scissor");
-	response = response.toLowerCase();
-	return response;
+function displayMessage(message) {
+	let results = document.querySelector('#resultsdiv');
+	let resultsContent = document.createElement('p');
+	resultsContent.textContent = message;
+	results.appendChild(resultsContent);
 }
 
-// rock beats scissor, rock loses to paper, rock ties rock
-// paper beats rock, paper loses to scissor, paper ties paper
-// scissor beats paper, scissor loses to rock, scissor ties scissor
-
 // Simulates one game between user and computer
-function playGame(playerSelection, computerSelection) { 
-
-	let tieMessage = `Tie Time!`;
-	let winMessage = `You win!`;
-	let loseMessage = `You lose!`
+function playGame(e) { 
+	let playerSelection = e.target.id;
+	let computerSelection = computerHand();
 
 	if (playerSelection == computerSelection) {
-		return tieMessage;
+		displayMessage(tieMessage);
+		return;
 	}
 
 	if (playerSelection == "rock") {
 		if (computerSelection == "scissor") {
-			return winMessage;
+			displayMessage(winMessage);
+			return;
 		} else {
-			return loseMessage;
+			displayMessage(loseMessage);
+			return;
 		} 
 	} 
 
 	if (playerSelection == "paper") {
 		if (computerSelection == "rock") {
-			return winMessage;
+			displayMessage(winMessage);
+			return;
 		} else {
-			return loseMessage;
+			displayMessage(loseMessage);
+			return;
 		}
 	 }
 
 	 if (playerSelection == "scissor") {
 	 	if (computerSelection == "paper") {
-	 		return winMessage;
+	 		displayMessage(winMessage);
 	 	} else {
-	 		return loseMessage;
+	 		displayMessage(loseMessage);
 	 	}
 	 }
 }
 
 // Keeps track of scores
 function playRound(result) {
-	
 	if (result == `You win!`) {
 		return 1;
 	} else if (result == `You lose!`) {
 		return -1;
 	} 
 }
+
+	// Simulate five consecutive games, recording each result and showing a winner at the end.
+	// function playSeries() { 
+	// 	let playerScore = 0; //
+	// 	let computerScore = 0; //
+
+	// 	let round1 = playGame(playerHand(), computerHand());
+	// 	if (round1 == `You win!`) {
+	// 		playerScore += 1;
+	// 	} else if (round1 == `You lose!`) {
+	// 		computerScore += 1;
+	// 	}
+	// 	console.log(round1);
+
+	// 	let round2 = playGame(playerHand(), computerHand());
+	// 	if (round2 == `You win!`) {
+	// 		playerScore += 1;
+	// 	} else if (round2 == `You lose!`) {
+	// 		computerScore += 1;
+	// 	}
+	// 	console.log(round2);
+
+	// 	let round3 = playGame(playerHand(), computerHand());
+	// 	if (round3 == `You win!`) {
+	// 		playerScore += 1;
+	// 	} else if (round3 == `You lose!`) {
+	// 		computerScore += 1;
+	// 	}
+	// 	console.log(round3);	
+
+	// 	let round4 = playGame(playerHand(), computerHand());
+	// 	if (round4 == `You win!`) {
+	// 		playerScore += 1;
+	// 	} else if (round4 == `You lose!`) {
+	// 		computerScore += 1;
+	// 	}
+	// 	console.log(round4);	
+
+	// 	let round5 = playGame(playerHand(), computerHand());
+	// 	if (round5 == `You win!`) {
+	// 		playerScore++;
+	// 	} else if (round5 == `You lose!`) {
+	// 		computerScore++;
+	// 	}
+	// 	console.log(round5);
+
+	// 	let score = "you: " + playerScore + " computer: " + computerScore;
+	// 	console.log(score);
+	// 	if (playerScore > computerScore) {
+	// 		console.log("you won the game!");
+	// 	} else if (playerScore < computerScore) {
+	// 		console.log("you lost to the machine");
+	// 	} else {
+	// 		console.log("It was a tie!");
+	// 	}
+	// }
+
+// console.log(playGame(playerHand(), computerHand()));
 
 
